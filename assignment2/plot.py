b77df55a38ee3e67fe9841471ddc3bc4824100f2
noaz <noaz@users.noreply.github.com>
2023-05-23 08:49:00 +0100

    update plot.py to plot a CDF

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -9,11 +9,14 @@ xlabel = 'xlabel'
 ylabel = 'ylabel'
 title='Simple plot'
 fig_name='test.png'
+bins=100 #adjust the number of bins to your plot
 
 
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
 
 plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+#Comment the line above and uncomment the line below to plot a CDF
+#plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
